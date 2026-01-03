3e59e560e2897eed8a4aae15fd9b081c8a9c7c87
oOccasio <dhzktldh@gmail.com>
2026-01-04 03:32:28 +0900

    Spring Initialize

---
+++ src/main/java/sopt/comfit/ComfitApplication.java
@@ -0,0 +1,13 @@
+package sopt.comfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComfitApplication.class, args);
+	}
+
+}
