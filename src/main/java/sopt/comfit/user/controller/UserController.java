f921d90f714fced790e93ce1804733542b0f1327
oOccasio <dhzktldh@gmail.com>
2026-01-08 20:34:17 +0900

    docs: UserSwagger Docs Write #10

--- src/main/java/sopt/comfit/user/controller/UserController.java
+++ src/main/java/sopt/comfit/user/controller/UserController.java
@@ -1,13 +1,12 @@
 package sopt.comfit.user.controller;
 
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import sopt.comfit.global.annotation.LoginUser;
 import sopt.comfit.global.dto.PageDto;
 import sopt.comfit.global.enums.ESort;
@@ -18,34 +17,28 @@ import sopt.comfit.user.service.UserService;
 @RestController
 @RequestMapping("/api/v1/me")
 @RequiredArgsConstructor
-public class UserController {
+public class UserController implements UserSwagger {
 
     private final UserService userService;
 
-    @GetMapping
-    @SecurityRequirement(name = "JWT")
+    @Override
     public GetMeResponseDto getMe (@LoginUser Long userId){
         return userService.getMe(userId);
     }
 
-    @PostMapping("/companies/{companyId}")
-    @SecurityRequirement(name = "JWT")
-    @ResponseStatus(HttpStatus.CREATED)
+    @Override
     public Long addBookmark(@LoginUser Long userId,
                             @PathVariable Long companyId){
         return userService.addBookmark(userId, companyId);
     }
 
-    @DeleteMapping("/companies/{companyId}")
-    @SecurityRequirement(name = "JWT")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Override
     public void removeBookmark(@LoginUser Long userId,
                                @PathVariable Long companyId) {
         userService.removeBookmark(userId, companyId);
     }
 
-    @GetMapping("/companies")
-    @SecurityRequirement(name = "JWT")
+    @Override
     public PageDto<GetBookmarkCompany> getBookmarkCompany(@LoginUser Long userId,
                                                           @RequestParam(defaultValue = "0") int page,
                                                           @RequestParam(defaultValue = "LATEST") ESort sort){
