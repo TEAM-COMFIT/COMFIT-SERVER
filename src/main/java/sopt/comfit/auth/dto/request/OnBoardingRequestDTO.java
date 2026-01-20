2561c8f36cd9ff81f414f550bd216fd2bef15580
oOccasio <dhzktldh@gmail.com>
2026-01-21 04:54:22 +0900

    fix: OnBoarding API #54

--- src/main/java/sopt/comfit/auth/dto/request/OnBoardingRequestDTO.java
+++ src/main/java/sopt/comfit/auth/dto/request/OnBoardingRequestDTO.java
@@ -1,13 +1,44 @@
 package sopt.comfit.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import sopt.comfit.global.enums.EIndustry;
+import sopt.comfit.user.domain.EEducationLevel;
+import sopt.comfit.user.domain.EJob;
+
 public record OnBoardingRequestDTO (
-        String educationLevel,
-        String firstIndustry,
-        String secondIndustry,
-        String thirdIndustry,
-        String firstJob,
-        String secondJob,
-        String thirdJob,
+
+        @NotNull
+        @Schema(example = "HIGH_SCHOOL")
+        EEducationLevel educationLevel,
+
+
+        @NotNull
+        @Schema(example = "IT")
+        EIndustry firstIndustry,
+
+        @NotNull
+        @Schema(example = "MEDIA_CONTENTS")
+        EIndustry secondIndustry,
+
+        @NotNull
+        @Schema(example = "RETAIL")
+        EIndustry thirdIndustry,
+
+        @NotNull
+        @Schema(example = "MARKETING_STRATEGY")
+        EJob firstJob,
+
+        @NotNull
+        @Schema(example = "BRAND_MARKETING")
+        EJob secondJob,
+
+        @NotNull
+        @Schema(example = "DIGITAL_MARKETING")
+        EJob thirdJob,
+
+        @NotNull
+        @Schema(example = "1")
         Long universityId
 ) {
 }
